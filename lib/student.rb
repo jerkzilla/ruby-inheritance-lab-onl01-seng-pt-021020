417bf50c081221a16bc568c5c1e4aa35be6a6215
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-11 12:45:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+#
+class Student < Teacher
+  attr_accessor :knowledge
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    knowledge << String
+  end
+  
+  
 end
